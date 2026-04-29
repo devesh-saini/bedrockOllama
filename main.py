87b593551a2188ffcbdcdbf710e931ce8734ae54
d3v3sh 54ini <128877193+devesh-saini@users.noreply.github.com>
2026-04-29 14:13:21 +0530

    AWS Knowledge Base integration successful

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import boto3
 import json
 
-bedrock_client = boto3.client(service_name='bedrock-runtime', region_name='eu-north-1')
+bedrock_client = boto3.client(service_name='bedrock-agent-runtime', region_name='eu-north-1')
 
 prompt = 'Help me set up networking between two docker containers.'
 
+knowledge_base_retrieval = bedrock_client.retrieve(
+    knowledgeBaseId='D9GSQGHM9G',
+    retrievalQuery={
+        "text": prompt
+    }
+)
+
+print(knowledge_base_retrieval['retrievalResults'])

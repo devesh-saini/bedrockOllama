ed37baff4823a421bbe8280b1530a9a956faaa00
d3v3sh 54ini <128877193+devesh-saini@users.noreply.github.com>
2026-05-06 21:23:35 +0530

    Speech to Text successfully implemented.

--- DockerRag/DockerRag/urls.py
+++ DockerRag/DockerRag/urls.py
@@ -14,15 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
-    path('about/', views.about),
-    path('tech_stack/', views.tech_stack),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('tech_stack/', views.tech_stack, name='tech_stack'),
     path('polly/', views.polly_speak, name='polly_speak'),
-    path('stream/', views.stream_response, name='stream')
-]   
+    path('stream/', views.stream_response, name='stream'),
+    path('transcribe/', views.transcribe_audio, name='transcribe'),
+]

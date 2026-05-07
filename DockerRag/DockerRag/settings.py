8a20bab24b0081477018ed3362c5e45def525152
d3v3sh 54ini <128877193+devesh-saini@users.noreply.github.com>
2026-05-07 15:12:30 +0530

    Added to allowed hosts.

--- DockerRag/DockerRag/settings.py
+++ DockerRag/DockerRag/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://web-production-bb89d.up.railway.app/', os.environ.get('ALLOWED_RAILWAY')]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'web-production-bb89d.up.railway.app', os.environ.get('ALLOWED_RAILWAY')]
 
 
 # Application definition
